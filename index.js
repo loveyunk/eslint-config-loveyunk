da1c11254284db4a63dd2f6193e1bc77bf27592c
zyw <mjjccc@126.com>
2019-11-05 17:55:08 +0800

    update rules

--- index.js
+++ index.js
@@ -10,5 +10,10 @@ module.exports = {
     ecmaVersion: 6,
     sourceType: 'module'
   },
-  urles: {}
+  urles: {
+    // error; 缩进2空格
+    indent: [2, 2],
+    // error; 结尾必须有分号
+    semi: [2, 'never']
+  }
 };
