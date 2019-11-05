34a3709a7e570d0d83841dca5817e401d292626d
zyw <mjjccc@126.com>
2019-11-05 17:30:54 +0800

    add index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es6: true,
+    node: true
+  },
+  extends: 'eslint:recommended',
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module'
+  },
+  urles: {}
+};
